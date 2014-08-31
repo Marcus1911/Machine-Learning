3b6a930f680383ca3809d0f8643ce100a51c5637
Marcus1911 <marcus.sandri@ufscar.br>
2014-08-31 20:12:07 -0300

    new read_pic using urllib

--- facebook/read_pic.py
+++ facebook/read_pic.py
@@ -1,9 +1,10 @@
-import urllib.request
-import json
+from bs4 import BeautifulSoup
+import urllib
+
+urlData = urllib.urlopen('https://www.facebook.com/photo.php?fbid=4414604421313&set=a.1299978197604.33162.1774899026&type=1&theater')
+data = str(urlData.readlines())
+bs = BeautifulSoup(data)
+imgUrl = bs.find('img', attrs={'class': 'fbPhotoImage img'}).get('src')
+urllib.urlretrieve(imgUrl, "somepicture.jpg")
 
 
-url = 'https://www.facebook.com/FRIEND/friends'
-resp = urllib.request.urlopen(url).read()
-data = json.loads(resp.decode('utf-8'))
-for amigo in data['data']:
-  print (amigo['name'])
